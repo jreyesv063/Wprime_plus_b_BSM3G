f0281ab4aebccafd16a557dc6b0a830f7e3384ce
jose reyes <j.reyesv@uniandes.edu.co>
2025-03-09 00:21:20 +0100

    c-tagging configs

---
+++ wprime_plus_b/selections/ztoll/ctag_config.py
@@ -0,0 +1,29 @@
+ztoll_cjet_config = {
+    "ll+c": {
+        "mu": {
+            "cjet_pt_threshold": 30,
+            "cjet_eta_threshold": 2.4,
+            "ctag_working_point": "T",
+            "cjet_id_wp": "tightLepVeto",
+            "cjet_pileup_id": "T"
+        },
+    },
+    "ll": {
+        "mu": {
+            "cjet_pt_threshold": 30,
+            "cjet_eta_threshold": 2.4,
+            "ctag_working_point": "T",
+            "cjet_id_wp": "tightLepVeto",
+            "cjet_pileup_id": "T"
+        },
+    },
+    "ll_ISR": {
+        "mu":{
+            "cjet_pt_threshold": 30,
+            "cjet_eta_threshold": 2.4,
+            "ctag_working_point": "T",
+            "cjet_id_wp": "tightLepVeto",
+            "cjet_pileup_id": "T"
+        },
+    }
+}
